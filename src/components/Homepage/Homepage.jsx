d6223acbd92503d7220114b9cba1b9559aa4f8d8
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-12 19:05:08 +0530

    section 2 complete

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Section1 from './Section1'
+import Section2 from './Section2'
 
 const Homepage = () => {
   return (
-    <div className='px-24 '>
+    <div className=' '>
       <Section1></Section1>
+      <Section2></Section2>
     </div>
   )
 }
