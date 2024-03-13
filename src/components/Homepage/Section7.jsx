bb9954b7b9b0c08a9f789b6a595c5a13c3ce7e14
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-13 12:05:39 +0530

    section7 UI done

--- src/components/Homepage/Section7.jsx
+++ src/components/Homepage/Section7.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import "./section7.scss";
+import Testimonial from "./Testimonial";
+
 const Section7 = () => {
+    const description=["This creative agency stands out with their exceptional talent and experstise.Their ability to think outside the box and bring unique ideas to life is truely impressive. With meticulous attention to details, they consistently deliver visually stunning and impactful work."]
   useEffect(() => {
     const selectedAvatar = document.querySelectorAll(".avatar");
     console.log("selected avatar", selectedAvatar);
@@ -13,7 +16,7 @@ const Section7 = () => {
   }, []);
 
   return (
-    <div className="section-7 py-24 px-24 flex gap-8 text-center items-center bg-slate-100 h-[700px] mt-16 flex-col">
+    <div className="section-7 py-24 px-24 flex gap-12 text-center items-center bg-slate-100 h-[700px] mt-16 flex-col">
       <div className="top">
         <h1 className="text-4xl mb-2 font-normal">
           <span className="font-semibold ">Customer</span> Voice :{" "}
@@ -23,7 +26,7 @@ const Section7 = () => {
         </h1>
       </div>
       <div className="mid-image-container flex gap-6 items-center ">
-        <div className="avatar object-cover rounded-[50%] flex justify-center items-center ">
+        <div className="avatar object-cover rounded-[50%] flex justify-center items-center active ">
           <img src="/av2.png" className="h-16 w-16 rounded-[50%] " alt="" />
         </div>
         <div className="avatar object-cover rounded-[50%] flex justify-center items-center mt-8">
@@ -44,6 +47,9 @@ const Section7 = () => {
         <div className=" avatar object-cover rounded-[50%] flex justify-center items-center">
           <img src="/av8.png" className="h-16 w-16 rounded-[50%]" alt="" />
         </div>
+      </div>
+      <div className="bottom-container">
+       <Testimonial name="Rajeev Kumar" country="India" description={description[0]}  ></Testimonial>
       </div>
     </div>
   );
---
+++ src/components/Homepage/Testimonial.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+const Testimonial = ({name,country,description}) => {
+  return (
+    <div className='testimonials flex flex-col justify-center items-center gap-6'>
+      <div className="upper flex flex-col gap-1 items-center justify-center">
+        <div className='symbol text-mainred '><FormatQuoteIcon></FormatQuoteIcon></div>
+        <h1 className='font-semibold text-xl tracking-wider'>{name}</h1>
+        <p className='text-gray-700 tracking-wider'>{country}</p>
+      </div>
+      <div className="lower text-center w-[60%] flex justify-center">
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial
