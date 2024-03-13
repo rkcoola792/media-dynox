324e2065ef7776ff3bec65ac7caf2acdeeaa2bcc
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-13 11:44:02 +0530

    selected feature implemented

--- src/components/Homepage/Section7.jsx
+++ src/components/Homepage/Section7.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import "./section7.scss"
+import React, { useEffect } from "react";
+import "./section7.scss";
 const Section7 = () => {
-    let circle = " border-red-500 border-4 rounded-[50%] h-20 w-20";
+  useEffect(() => {
+    const selectedAvatar = document.querySelectorAll(".avatar");
+    console.log("selected avatar", selectedAvatar);
+
+    selectedAvatar.forEach((ele) =>
+      ele.addEventListener("click", () => {
+        document.querySelector(".active")?.classList.remove('active')
+        ele.classList.add("active")})
+    );
+  }, []);
+
   return (
     <div className="section-7 py-24 px-24 flex gap-8 text-center items-center bg-slate-100 h-[700px] mt-16 flex-col">
       <div className="top">
@@ -13,30 +23,30 @@ const Section7 = () => {
         </h1>
       </div>
       <div className="mid-image-container flex gap-6 items-center ">
-        <div className="object-cover rounded-[50%] flex justify-center items-center ">
+        <div className="avatar object-cover rounded-[50%] flex justify-center items-center ">
           <img src="/av2.png" className="h-16 w-16 rounded-[50%] " alt="" />
         </div>
-        <div className=" object-cover rounded-[50%] flex justify-center items-center mt-8">
+        <div className="avatar object-cover rounded-[50%] flex justify-center items-center mt-8">
           <img src="/av3.webp" className="h-16 w-16 rounded-[50%] " alt="" />
         </div>
-        <div className=" object-cover rounded-[50%] flex justify-center items-center ">
+        <div className="avatar object-cover rounded-[50%] flex justify-center items-center ">
           <img src="/av4.jpg" className="h-16 w-16 rounded-[50%]" alt="" />
         </div>
-        <div className="  object-cover rounded-[50%] mt-8 ">
+        <div className="avatar object-cover rounded-[50%] mt-8 flex justify-center items-center">
           <img src="/av9.png" className="h-16 w-16 rounded-[50%]" alt="" />
         </div>
-        <div className="  object-cover rounded-[50%] ">
+        <div className="avatar object-cover rounded-[50%] flex justify-center items-center">
           <img src="/av6.png" className="h-16 w-16 rounded-[50%]" alt="" />
         </div>
-        <div className="  object-cover rounded-[50%] mt-8 ">
+        <div className=" avatar object-cover rounded-[50%] mt-8 flex justify-center items-center">
           <img src="/av7.png" className="h-16 w-16 rounded-[50%]" alt="" />
         </div>
-        <div className="  object-cover rounded-[50%] ">
+        <div className=" avatar object-cover rounded-[50%] flex justify-center items-center">
           <img src="/av8.png" className="h-16 w-16 rounded-[50%]" alt="" />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Section7
+export default Section7;
