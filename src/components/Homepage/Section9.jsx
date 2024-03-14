769730814e406daa14437823da85749310c7c4ae
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-14 23:03:32 +0530

    section 9 compltetd

--- src/components/Homepage/Section9.jsx
+++ src/components/Homepage/Section9.jsx
@@ -5,11 +5,11 @@ const Section9 = () => {
   return (
     <div className="relative w-full h-full flex justify-center mt-24">
       <img src="/cta-banner.png" alt="" />
-      <h1 className="absolute text-5xl text-white text-center top-24 leading-[70px] w-[45%] font-thin">
+      <h1 className="absolute  md:text-5xl sm:text-3xl  text-white text-center lg:top-28 sm:top-20 leading-[70px] w-[50%] font-thin">
         <span className="font-bold">Lets discuss</span> make something cool
         together
       </h1>
-      <div className='absolute bottom-28'>
+      <div className='absolute lg:bottom-28 sm:bottom-16'>
         <Explore text="Get in touch" color="white"></Explore>
       </div>
     </div>
