75b800b0d875a921b877c085efb6cdb30040cf68
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-16 13:42:08 +0530

    left footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,33 +10,35 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Footer = () => {
   return (
-    <div className="footer mt-32 h-[500px] flex ">
-      <div className="left bg-[#131111] w-[70%] ">
-        <div className="top flex lg:gap-[90px] sm:gap-12 justify-center pt-24 px-12">
+    <div className="footer mt-32 sm:h-[500px] sm:flex">
+      <div className="left bg-[#131111] sm:w-[70%] pb-24">
+        <div className="top sm:flex lg:gap-[90px] sm:gap-12 justify-center sm:pt-24 px-8 py-1">
           <div>
-            <h1 className="lg:text-3xl sm:text-2xl  sm:hidden lg:block font-semibold text-white mt-20">
+            <h1 className="lg:text-3xl sm:text-2xl sm:hidden lg:block font-semibold text-white mt-20 text-4xl">
               MediaDynox<span className="font-bold text-mainred">.</span>
             </h1>
           </div>
-          <div className="services flex flex-col">
-            <h1 className="title text-white text-2xl mb-6">Services</h1>
-            <div className="services-elemets flex flex-col gap-4 text-white">
-              <h3 className="opacity-60 hover:opacity-100 cursor-pointer transition duration-450 ease-out hover:ease-in ">
+          <div className="services flex flex-col my-8 sm:my-0">
+            <h1 className="title text-white sm:text-2xl text-3xl mb-6">
+              Services
+            </h1>
+            <div className="services-elemets flex flex-col gap-4 text-white ">
+              <h3 className="opacity-60 hover:opacity-100 cursor-pointer transition duration-450 ease-out hover:ease-in text-xl sm:text-base">
                 Design
               </h3>
-              <h3 className="opacity-60 hover:opacity-100 cursor-pointer transition duration-450 ease-out hover:ease-in ">
+              <h3 className="opacity-60 hover:opacity-100 cursor-pointer transition duration-450 ease-out hover:ease-in  text-xl sm:text-base">
                 Technology
               </h3>
-              <h3 className="opacity-60 hover:opacity-100 cursor-pointer transition duration-450 ease-out hover:ease-in">
+              <h3 className="opacity-60 hover:opacity-100 cursor-pointer transition duration-450 ease-out hover:ease-in  text-xl sm:text-base">
                 Business
               </h3>
-              <h3 className="opacity-60 hover:opacity-100 transition duration-450 ease-out hover:ease-in cursor-pointer">
+              <h3 className="opacity-60 hover:opacity-100 transition duration-450 ease-out hover:ease-in cursor-pointer  text-xl sm:text-base">
                 Marketing
               </h3>
             </div>
           </div>
           <div className="contact flex flex-col w-[250px]">
-            <h1 className="text-white text-2xl mb-6">Contact</h1>
+            <h1 className="text-white sm:text-xl mb-6 text-3xl">Contact</h1>
             <div className="contact-elements flex flex-col gap-2">
               <div className="address flex gap-2 ">
                 <div className="text-mainred opacity-70">
@@ -52,7 +54,7 @@ const Footer = () => {
                 <div className="text-mainred opacity-70 ">
                   <LocalPhoneIcon></LocalPhoneIcon>
                 </div>
-                <p className=" text-white opacity-60 hover:opacity-100 transition duration-450 ease-out hover:ease-in ">
+                <p className=" text-white opacity-60 hover:opacity-100 transition duration-450 ease-out hover:ease-in sm:text-base">
                   +91 92979 39393
                 </p>
               </div>
@@ -68,32 +70,38 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="bottom lg:px-24 sm:pl-24 ">
-          <div className="upper flex gap-2 mt-16 items-center">
+        <div className="bottom lg:px-24 sm:pl-24 px-8 ">
+          <div className="upper sm:flex-row flex flex-col gap-2 mt-16 sm:items-center">
             <div className=" text-sm text-white opacity-60 hover:opacity-90 cursor-pointer transition duration-450 ease-out hover:ease-in ">
               Privacy Policy{" "}
             </div>
-            <div className="text-sm text-white opacity-60 ">|</div>
+            <div className="text-sm text-white opacity-60 hidden sm:block ">
+              |
+            </div>
             <div className="text-sm text-white opacity-60 hover:opacity-90 cursor-pointer transition duration-450 ease-out hover:ease-in ">
               Terms and conditions{" "}
             </div>
-            <div className="text-sm text-white opacity-60 ">|</div>
+            <div className="text-sm text-white opacity-60 hidden sm:block">
+              |
+            </div>
             <div className=" text-sm text-white opacity-60 hover:opacity-90 cursor-pointer transition duration-450 ease-out hover:ease-in ">
               Privacy Shield Policy
             </div>
           </div>
-          <div className="lower mt-4">
+          <div className="lower sm:mt-4 mt-8">
             <p className="text-sm text-white opacity-60 ">
               Copyright@2024 Mediadynox.com. All Rights Reserved.
             </p>
           </div>
         </div>
       </div>
-     
-      <div className="right w-[35%] bg-slate-100 flex flex-col lg:pl-16 sm:pl-10 pt-24  ">
+
+      <div className="right sm:w-[35%] bg-slate-100 flex flex-col lg:pl-16 sm:pl-10 pt-24  ">
         <div className="top flex flex-col gap-2 ">
           <div className="title">
-            <h1 className="lg:text-4xl md:text-3xl font-medium">Get In Touch</h1>
+            <h1 className="lg:text-4xl md:text-3xl font-medium">
+              Get In Touch
+            </h1>
           </div>
           <div className="hr border border-b-black mt-1  w-[70%]">
             <hr />
@@ -102,7 +110,7 @@ const Footer = () => {
             <div className="md:scale-90 ">
               <Explore text="Contact Us" size="sm"></Explore>
             </div>
-            <div className='md:scale-90 '>
+            <div className="md:scale-90 ">
               <Explore text="Schedule a meeting" size="sm"></Explore>
             </div>
           </div>
