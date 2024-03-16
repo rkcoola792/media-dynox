27c68cfccc6b183164477ba0a360d84b70e363ec
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-03-16 15:18:27 +0530

    power tools resposive

--- src/components/Marketing/Section6.jsx
+++ src/components/Marketing/Section6.jsx
@@ -2,20 +2,50 @@ import React from 'react'
 
 const Section6 = () => {
   return (
-    <div className="section-6 flex justify-between items-center mt-32 pl-28  ">
+    <div className="section-6 sm:flex sm:justify-between py-12 sm:items-center sm:my-24 sm:pl-28 px-8  ">
       <div className="left-text md:w-[30%] sm:w-[50%]">
         {" "}
-        <h1 className="font-light lg:text-4xl sm:text-3xl leading-[45px]">
+        <h1 className="font-light lg:text-4xl sm:text-3xl  text-3xl leading-[45px]">
           <span className="font-semibold">Popular tool that power</span> our
           digital marketing services
         </h1>
       </div>
-      <div className="right-images flex flex-wrap  gap-6 lg:w-[40%] sm:w-[50%] ">
-        <div><img src="/GA.webp" className='object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px]'  alt="" /></div>
-        <div><img src="/webmaster.png" className='object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px]'  alt="" /></div>
-        <div><img src="/ads.png" className='object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px]'  alt="" /></div>
-        <div><img src="/semrush.png" className='object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px]'  alt="" /></div>
-        <div><img src="/hootsuite.png" className='object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px]'  alt="" /></div>
+      <div className=" right-images mt-12 sm:mt-0 flex flex-wrap justify-start items-start gap-6 lg:w-[40%] sm:w-[50%]">
+        <div>
+          <img
+            src="/GA.webp"
+            className="object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px] h-[75px] w-[150px]"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="/webmaster.png"
+            className="object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px] h-[75px] w-[150px]"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="/ads.png"
+            className="object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px] h-[75px] w-[150px] -ml-6 sm:ml-0"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="/semrush.png"
+            className="object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px] h-[75px] w-[150px] ml-6 sm:ml-0 "
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="/hootsuite.png"
+            className="object-contain sm:w-[75px] sm:h-[35px] md:w-[100px] md:h-[50px] h-[75px] w-[150px]"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
